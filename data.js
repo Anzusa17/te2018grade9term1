f7a11bf423e496e2fb515c0242d22fdae0aa6aca
Anzusa <anzusa.hernandez@csedge.org>
2018-10-06 15:51:46 -0400

    i completed challenge one

--- data.js
+++ data.js
@@ -1,5 +1,25 @@
 // Example
-let message = "Hello World!";
-console.log(message);
+let a=8;
+console.log(a);
+let b=27;
+console.log(b);
+let c=18;
+console.log(c);
+let billy="how are you?";
+console.log(billy)
+let ideas="okay";
+console.log(ideas);
+let hey="good";
+console.log(hey);
+let isTired= true;
+let likesSleep= true;
+let hatesGoogle= false;
+console.log(typeof isTired);
+console.log(typeof ideas);
+const READLINE= require("readline-sync");
+let userInput= READLINE.question("hello");
+console.log('hello:${userInput}');
+
+
 
 // Type your code below!
